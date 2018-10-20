384e3a58a3b059c8899d2594e84c2dfb99b2cf96
Earl0fPudding <weidenauermartin@gmail.com>
2018-10-20 12:49:58 +0200

    Print always current year in footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="page-footer footer-copyright grey lighten-4">
 	<div class="row">
-		<div class="grey-text text-darken-1 offset-s1 col s4">© 2017-2018 Kretschmayer, Steindl, Weidenauer</div>
+		<div class="grey-text text-darken-1 offset-s1 col s4">© 2017-{{ date("Y") }} Kretschmayer, Steindl, Weidenauer</div>
 		<a href="#" class="grey-text text-darken-1 offset-s4 col s1">Zur Dokumentation</a>
 	</div>
 </footer>
